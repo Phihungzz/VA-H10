4c8491426d2613e451c95d66eac6cefc77c5c78f
Phihungzz <153632529+Phihungzz@users.noreply.github.com>
2025-06-25 21:07:30 +0700

    Delete backend H10 directory

--- backend H10/models/Message.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const MessageSchema = new mongoose.Schema({
-  user: String,
-  prompt: String,
-  reply: String,
-  createdAt: { type: Date, default: Date.now }
-});
-export default mongoose.models.Message || mongoose.model('Message', MessageSchema);
--- backend H10/models/User.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  name: String,
-  password: String,
-});
-export default mongoose.models.User || mongoose.model('User', UserSchema);
